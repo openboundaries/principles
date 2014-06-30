f3916dde2f36e84008425de33062038f74cf8e71
Owen Scott <owen.m.scott@gmail.com>
2014-06-30 09:46:35 -0400

    clearer rendering code

--- js/main.js
+++ js/main.js
@@ -1,65 +1,49 @@
-//note: will fix excessive string concatenation shortly 
+//note: _ and $ assumed present because of preceeding script tags
 
-var CRITERIA_DIV_ID = 'criteria',
-	RATINGS_DIV_ID = 'ratings',
-	bulletTemplate,
-	sourceTitleTemplate,
-	ratingsTemplate,
-	ratingsHeaderTemplate;
+var CRITERIA_DIV_ID = '#criteria',
+	RATINGS_DIV_ID = '#ratings',
+	bulletTemplate = _.template('<li><strong><%=shortTitle%></strong>: <%=principle%></li>'),
+	sourceTitleTemplate = _.template('<td><a href="<%=url%>"><%=name%></a></td>'),
+	ratingsTemplate = _.template('<td title="<%=rationale%>" class=" <% if (rating === \'good\' ) { %> success <%} else if (rating === \'ok\') {%>warning<%} else if (rating === \'needs improvement\') {%>danger<%}%>"><%=rating%></td>'),
+	ratingsHeaderTemplate = _.template('<th><%=shortTitle%></th>');
 
-bulletTemplate = _.template('<li><strong><%=shortTitle%></strong>: <%=principle%></li>');
-
-ratingsHeaderTemplate = _.template('<th><%=shortTitle%></th>');
-
-sourceTitleTemplate = _.template('<td><a href="<%=url%>"><%=name%></a></td>');
-
-ratingsTemplate = _.template('<td class=" <% if (rating === \'good\' ) { %> success <%} else if (rating === \'ok\') {%>warning<%} else if (rating === \'needs improvement\') {%>danger<%}%>"><%=rating%></td>');
-
-
+//load criteria and data source data from json files and use to render markup
 loadJson('./data/criteria.json', function(criteria) {
 	
 	loadJson('./data/sources.json', function(sources) {
 
-		var criteriaString = '',
-			ratingsString = '',
-			ratingKeys;
+		var bulletList = $('<ul></ul>'),
+			ratingTable = $('<table class="table table-striped table-bordered"></table>'),
+			tableHeaderRow = $('<tr></tr>'),
+			criteriaKeys = _.keys(criteria);
 
-		ratingKeys = _.pluck(criteria, 'key');
+		tableHeaderRow.append('<th>Data Source</th>');
 
-		//create bullet point list
-		_.each(criteria, function(criteria) {
-			criteriaString += bulletTemplate(criteria)
+		//create bullet point list and table header row from criteria keys
+		_.each(criteriaKeys, function(key) {
+			bulletList.append(bulletTemplate(criteria[key]));
+			tableHeaderRow.append(ratingsHeaderTemplate(criteria[key]));
 		});
 
-		//create rating table headers
-		ratingsString += '<tr><th>Data Source</th>';
-		_.each(ratingKeys, function(key) {
-			ratingsString += ratingsHeaderTemplate(_.findWhere(criteria, {key:key}));
-		})
+		tableHeaderRow.appendTo(ratingTable);
 
-		ratingsString += '</tr>';
-
-		//create rating table	
+		//append list of sources and their respective ratings to table
 		_.each(sources, function(source) {
-			var sourceString = '<tr>',
-				r;
-			sourceString += sourceTitleTemplate(source);
-			_.each(ratingKeys, function(key) {
-				sourceString += ratingsTemplate(source.ratings[key]);
-			})
-			sourceString += '</tr>';
-			ratingsString += sourceString;
+			var row = $('<tr></tr>');
+			row.append(sourceTitleTemplate(source));
+			_.each(criteriaKeys, function(key) {
+				row.append(ratingsTemplate(source.ratings[key]));
+			});
+			row.appendTo(ratingTable);
 		})
 
-
-		document.getElementById(CRITERIA_DIV_ID).innerHTML = criteriaString;
-		document.getElementById(RATINGS_DIV_ID).innerHTML = ratingsString;
+		//append everything to the DOM
+		bulletList.appendTo(CRITERIA_DIV_ID);
+		ratingTable.appendTo(RATINGS_DIV_ID);
+		$(document).tooltip();
 
 	})
 });
-
-
-
 
 function loadJson(path, callback) {
 	
